c3fc1748ca66f5a3a3975702ec6e6045bd2f3528
Strelnikova Liubov <lubov0strelnikova@gmail.com>
2023-03-27 20:51:18 +0300

    add allure report

--- demoqa/tests/test_student_registration.py
+++ demoqa/tests/test_student_registration.py
@@ -1,5 +1,6 @@
 from demoqa.model.pages.registration_page import RegistrationPage
 from demoqa.data.students import Student
+import allure
 
 student = Student(first_name='Иван', last_name='Свиридов', user_email='IvanSviridov@gmail.com', gender='Male',
                   user_number='1234567891',
@@ -8,20 +9,25 @@ student = Student(first_name='Иван', last_name='Свиридов', user_emai
                   day_of_birth='11', month_of_birth='May', year_of_birth='1999', state='NCR', city='Delhi')
 
 
-def test_student_registration_form():
+@allure.title("Successful fill form")
+def test_student_registration_form(setup_browser):
     registration_page = RegistrationPage()
-    registration_page.open()
-    registration_page.registration_student(student)
+    with allure.step("Open registrations form"):
+        registration_page.open()
 
-    registration_page.should_registered_user_with(
-        'Иван Свиридов',
-        'IvanSviridov@gmail.com',
-        'Male',
-        '1234567891',
-        '11 May,1999',
-        'Computer Science',
-        'Reading',
-        'foto.jpg',
-        'Moscowskaya Street 18',
-        'NCR Delhi',
-    )
+    with allure.step("Fill form"):
+        registration_page.registration_student(student)
+
+    with allure.step("Check form results"):
+        registration_page.should_registered_user_with(
+            'Иван Свиридов',
+            'IvanSviridov@gmail.com',
+            'Male',
+            '1234567891',
+            '11 May,1999',
+            'Computer Science',
+            'Reading',
+            'foto.jpg',
+            'Moscowskaya Street 18',
+            'NCR Delhi',
+        )
