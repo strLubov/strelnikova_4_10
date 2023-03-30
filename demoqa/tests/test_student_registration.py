9dc73c501386480a776f0adfa280b9a6c4312aa5
Strelnikova Liubov <lubov0strelnikova@gmail.com>
2023-03-30 18:57:37 +0300

    удалила лишний код

--- demoqa/tests/test_student_registration.py
+++ demoqa/tests/test_student_registration.py
@@ -1,6 +1,5 @@
 from demoqa.model.pages.registration_page import RegistrationPage
 from demoqa.data.students import Student
-import allure
 
 student = Student(first_name='Иван', last_name='Свиридов', user_email='IvanSviridov@gmail.com', gender='Male',
                   user_number='1234567891',
@@ -9,25 +8,19 @@ student = Student(first_name='Иван', last_name='Свиридов', user_emai
                   day_of_birth='11', month_of_birth='May', year_of_birth='1999', state='NCR', city='Delhi')
 
 
-@allure.title("Successful fill form")
 def test_student_registration_form():
     registration_page = RegistrationPage()
-    with allure.step("Open registrations form"):
-        registration_page.open()
-
-    with allure.step("Fill form"):
-        registration_page.registration_student(student)
-
-    with allure.step("Check form results"):
-        registration_page.should_registered_user_with(
-            'Иван Свиридов',
-            'IvanSviridov@gmail.com',
-            'Male',
-            '1234567891',
-            '11 May,1999',
-            'Computer Science',
-            'Reading',
-            'foto.jpg',
-            'Moscowskaya Street 18',
-            'NCR Delhi',
-        )
+    registration_page.open()
+    registration_page.registration_student(student)
+    registration_page.should_registered_user_with(
+        'Иван Свиридов',
+        'IvanSviridov@gmail.com',
+        'Male',
+        '1234567891',
+        '11 May,1999',
+        'Computer Science',
+        'Reading',
+        'foto.jpg',
+        'Moscowskaya Street 18',
+        'NCR Delhi',
+    )
